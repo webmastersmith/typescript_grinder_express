38836278715f061171da2de7942edccedd82c387
Bryon Smith <bryon@smithauto.us>
2022-01-06 10:50:45 -0600

    setup routes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-console.log('yay!!')
-console.log('hello')
+import express, { Request, Response } from 'express'
+import { router } from './routes/loginRoutes'
+
+const port = 3000
+const app = express()
+
+// app.use(express.urlencoded({extended: true}
+
+app.use(router)
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,22 @@
+//express router
+import { Router, Request, Response } from 'express'
+
+const router = Router()
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+        <div>
+            <label>Email</label>
+            <input name="Email" />
+        </div>
+        <div>
+            <label>Password</label>
+            <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+    </form>
+  `)
+})
+
+export { router }
